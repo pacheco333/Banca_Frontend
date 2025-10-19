86dc1f784315b13b71dd07593c281706f2710064
Pacheco333 <santiago2006ortizp@gmail.com>
2025-10-18 23:59:59 -0500

    Solicitar producto como asesor en proceso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { roleGuard } from './core/guards/role.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/asesor',
+    pathMatch: 'full'
+  },
+  {
+    path: 'asesor',
+    loadChildren: () => import('./features/asesor/asesor.routes').then(m => m.ASESOR_ROUTES),
+    canActivate: [roleGuard],
+    data: { role: 'cajero' }
+  }
+  // Aquí agregarás asesor y admin después
+];
---
+++ src/app/features/asesor/components/solicitar-producto/solicitar-producto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitar-producto',
+  imports: [],
+  templateUrl: './solicitar-producto.component.html',
+  styleUrl: './solicitar-producto.component.css'
+})
+export class SolicitarProducto {
+
+}
