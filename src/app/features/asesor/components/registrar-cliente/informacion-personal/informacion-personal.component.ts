a512329571de44e3bf03ef3f684624cf83bcad8f
unknown <SENA@PALDFPCBIFSP221.FormacionSena.RED>
2025-10-27 17:29:43 -0500

    frontend

--- src/app/features/asesor/components/registrar-cliente/informacion-personal/informacion-personal.component.ts
+++ src/app/features/asesor/components/registrar-cliente/informacion-personal/informacion-personal.component.ts
@@ -1,4 +1,4 @@
- import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -9,31 +9,67 @@ import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angula
   templateUrl: './informacion-personal.component.html',
 })
 export class InformacionPersonalComponent implements OnInit {
-  @Input() datosPersonales: any; // 👈 agrega esto
-  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() datosPersonales: any = {};
+  @Output() datosPersonalesChange = new EventEmitter<any>();
+
   form!: FormGroup;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.fb.group({
-      tipo_documento: ['', Validators.required],
-      numero_documento: ['', Validators.required],
-      lugar_expedicion: [''],
-      ciudad_nacimiento: [''],
-      fecha_nacimiento: ['', Validators.required],
-      fecha_expedicion: [''],
-      primer_nombre: ['', Validators.required],
-      segundo_nombre: [''],
-      primer_apellido: ['', Validators.required],
-      segundo_apellido: [''],
-      genero: ['', Validators.required],
-      nacionalidad: ['', Validators.required],
-      otra_nacionalidad: [''],
-      estado_civil: ['', Validators.required],
-      grupo_etnico: ['', Validators.required],
+      tipoDocumento: [this.datosPersonales.tipoDocumento || '', Validators.required],
+      numeroDocumento: [this.datosPersonales.numeroDocumento || '', Validators.required],
+      nombre: [this.datosPersonales.nombre || '', Validators.required],
+      apellido: [this.datosPersonales.apellido || '', Validators.required],
+      genero: [this.datosPersonales.genero || '', Validators.required],
+      estadoCivil: [this.datosPersonales.estadoCivil || '', Validators.required],
+      nacionalidad: [this.datosPersonales.nacionalidad || '', Validators.required],
     });
 
-    this.formReady.emit(this.form);
+    // Emitir los cambios al padre cada vez que cambie algo en el formulario
+    this.form.valueChanges.subscribe(value => {
+      this.datosPersonalesChange.emit(value);
+    });
   }
 }
+
+//  import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-informacion-personal',
+//   standalone: true,
+//   imports: [CommonModule, ReactiveFormsModule],
+//   templateUrl: './informacion-personal.component.html',
+// })
+// export class InformacionPersonalComponent implements OnInit {
+//   @Input() datosPersonales: any; // 👈 agrega esto
+//   @Output() formReady = new EventEmitter<FormGroup>();
+//   form!: FormGroup;
+
+//   constructor(private fb: FormBuilder) {}
+
+//   ngOnInit() {
+//     this.form = this.fb.group({
+//       tipo_documento: ['', Validators.required],
+//       numero_documento: ['', Validators.required],
+//       lugar_expedicion: [''],
+//       ciudad_nacimiento: [''],
+//       fecha_nacimiento: ['', Validators.required],
+//       fecha_expedicion: [''],
+//       primer_nombre: ['', Validators.required],
+//       segundo_nombre: [''],
+//       primer_apellido: ['', Validators.required],
+//       segundo_apellido: [''],
+//       genero: ['', Validators.required],
+//       nacionalidad: ['', Validators.required],
+//       otra_nacionalidad: [''],
+//       estado_civil: ['', Validators.required],
+//       grupo_etnico: ['', Validators.required],
+//     });
+
+//     this.formReady.emit(this.form);
+//   }
+// }
--- src/app/features/asesor/components/registrar-cliente/registrar-cliente.component.ts
+++ src/app/features/asesor/components/registrar-cliente/registrar-cliente.component.ts
@@ -8,7 +8,6 @@ import { InformacionFactacComponent } from './informacion-factca/informacion-fac
 import { AsesorService } from '../../services/asesor.service';
 import { MatTabsModule } from '@angular/material/tabs';
 
-
 @Component({
   selector: 'app-registrar-cliente',
   standalone: true,
@@ -19,31 +18,84 @@ import { MatTabsModule } from '@angular/material/tabs';
     InformacionPersonalComponent,
     ContactoPersonalComponent,
     InformacionLaboralComponent,
-    InformacionFactacComponent
+    InformacionFactacComponent,
   ],
   templateUrl: './registrar-cliente.component.html',
-  styleUrls: ['./registrar-cliente.component.css']
+  styleUrls: ['./registrar-cliente.component.css'],
 })
 export class RegistrarClienteComponent {
-  datosPersonales = { tipoDocumento: '', numeroDocumento: '', nombre: '', apellido: '' };
+  datosPersonales = {
+    tipoDocumento: '',
+    numeroDocumento: '',
+    nombre: '',
+    apellido: '',
+    genero: '',
+    estadoCivil: '',
+    nacionalidad: '',
+  };
   datosContacto = { telefono: '', email: '', direccion: '' };
   datosLaborales = { ocupacion: '', ingresos: '', empresa: '' };
   datosFactac = { ocupacion: '', ingresos: 0, empresa: '' };
-  
 
   constructor(private asesorService: AsesorService) {}
 
   registrarCliente() {
-    const nuevoCliente = {
-      ...this.datosPersonales,
-      ...this.datosContacto,
-      ...this.datosLaborales,
-      ...this.datosFactac
+    // Estructuramos el payload tal como lo espera el backend
+    const payload = {
+      // Información personal
+      tipoDocumento: this.datosPersonales.tipoDocumento,
+      numeroDocumento: this.datosPersonales.numeroDocumento,
+      primerNombre: this.datosPersonales.nombre,
+      primerApellido: this.datosPersonales.apellido,
+      genero: this.datosPersonales.genero,
+      estadoCivil: this.datosPersonales.estadoCivil,
+      nacionalidad: this.datosPersonales.nacionalidad,
+
+      // Contacto personal
+      contacto: {
+        direccion: this.datosContacto.direccion,
+        telefono: this.datosContacto.telefono,
+        correo: this.datosContacto.email,
+      },
+
+      // Información laboral
+      laboral: {
+        nombreEmpresa: this.datosLaborales.empresa,
+        ingresos: this.datosLaborales.ingresos,
+        ocupacion: this.datosLaborales.ocupacion,
+      },
+
+      // FACTA / CRS (por ahora con valores por defecto)
+      facta: {
+        esResidenteExtranjero: false,
+        pais: 'Colombia',
+      },
     };
 
-    this.asesorService.registrarCliente(nuevoCliente).subscribe({
-      next: () => alert('Cliente registrado correctamente'),
-      error: (err) => console.error('Error al registrar cliente', err)
+    // Llamada al servicio
+    this.asesorService.registrarCliente(payload).subscribe({
+      next: (res) => {
+        console.log('Respuesta del servidor:', res);
+        alert('✅ Cliente registrado correctamente');
+      },
+      error: (err: any) => {
+        console.error('❌ Error al registrar cliente:', err);
+        alert('Ocurrió un error al registrar el cliente');
+      },
     });
   }
+
+  // registrarCliente() {
+  //   const nuevoCliente = {
+  //     ...this.datosPersonales,
+  //     ...this.datosContacto,
+  //     ...this.datosLaborales,
+  //     ...this.datosFactac
+  //   };
+
+  //   this.asesorService.registrarCliente(nuevoCliente).subscribe({
+  //     next: () => alert('Cliente registrado correctamente'),
+  //     error: (err) => console.error('Error al registrar cliente', err)
+  //   });
+  // }
 }
